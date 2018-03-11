c3b49635bc82e2f881df884b4b9e9de036e44634
Pat Kennedy <PatKenn987@gmail.com>
2018-03-11 11:26:37 -0400

    Add files via upload

---
+++ ESP8266MQTTTempMonTwoProbes.ino
@@ -0,0 +1,75 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+//#include <OneWire.h>
+#include <DallasTemperature.h>
+
+//Variable to store the number of temperature probes in the system.
+int DevNum = 0;
+
+//MQTT Broker
+//const char* mqtt_server = "broker.mqtt-dashboard.com";
+const char* mqtt_server = "192.168.1.233";
+const char* MqttClientID = "ESP8266Client";
+
+char msg[75];
+
+// Replace with your network details
+//const char* ssid = "FiOS-M07WY-Guest";
+//const char* password = "Kennedy987";
+const char* ssid = "FiOS-M07WY";
+const char* password = "toe6723ear0906rock";
+
+const char* user_name = "PJK";
+const char* pass = "SkiHaus";
+
+//Main Loop Timing
+long lastMsg = 0;
+int value = 0;
+
+WiFiClient espClient;
+
+PubSubClient client(mqtt_server, 1883, espClient);
+
+//------------------------------------------
+void setup() {
+  //Setup Serial port speed
+  Serial.begin(115200);
+  //Setup DS18b20 temperature sensor
+  SetupDS18B20();
+  DevNum = GetNumOfTempDevices();
+
+  setup_wifi();
+  
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  long now = millis();
+  //MQTT Loop
+  if(!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
+  TempLoop( now );
+/*
+  for(int i=0; i<DevNum; i++){
+      Serial.print("Temp");
+      Serial.print(i);
+      Serial.print("= ");
+      Serial.println(GetCurrentTemp(i));
+   }*/
+
+    if(now-lastMsg >2000){
+      lastMsg = now;
+      ++value;
+      //snprintf (msg, 75, "hello word #%ld", value);
+      snprintf(msg,75,"testing/mqtt/topic = #%f", GetCurrentTemp(0));
+      Serial.print("Publish message: ");
+      Serial.println(msg);
+      client.publish("TempOut",msg);
+  }
+  delay(3000);  
+}
---
+++ TempMeasurement.ino
@@ -0,0 +1,99 @@
+
+//------------------------------------------
+//DS18B20
+#define ONE_WIRE_BUS D1 //Pin to which is attached a temperature sensor
+#define ONE_WIRE_MAX_DEV 15 //The maximum number of devices
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+int numberOfDevices; //Number of temperature devices found
+DeviceAddress devAddr[ONE_WIRE_MAX_DEV];  //An array device temperature sensors
+float tempDev[ONE_WIRE_MAX_DEV]; //Saving the last measurement of temperature
+float tempDevLast[ONE_WIRE_MAX_DEV]; //Previous temperature measurement
+long lastTemp = 0; //The last measurement
+const int durationTemp = 5000; //The frequency of temperature measurement
+
+
+int GetNumOfTempDevices(void)
+{
+  return numberOfDevices;
+}
+
+float GetCurrentTemp(int DeviceNum)
+{
+  return tempDev[DeviceNum];
+}
+
+//------------------------------------------
+//Convert device id to String
+String GetAddressToString(DeviceAddress deviceAddress){
+  String str = "";
+  for (uint8_t i = 0; i < 8; i++){
+    if( deviceAddress[i] < 16 ) str += String(0, HEX);
+    str += String(deviceAddress[i], HEX);
+  }
+  return str;
+}
+
+//Setting the temperature sensor
+void SetupDS18B20(){
+  DS18B20.begin();
+
+  Serial.print("Parasite power is: "); 
+  if( DS18B20.isParasitePowerMode() ){ 
+    Serial.println("ON");
+  }else{
+    Serial.println("OFF");
+  }
+  
+  numberOfDevices = DS18B20.getDeviceCount();
+  Serial.print( "Device count: " );
+  Serial.println( numberOfDevices );
+
+  lastTemp = millis();
+  DS18B20.requestTemperatures();
+
+  // Loop through each device, print out address
+  for(int i=0;i<numberOfDevices; i++){
+    // Search the wire for address
+    if( DS18B20.getAddress(devAddr[i], i) ){
+      //devAddr[i] = tempDeviceAddress;
+      Serial.print("Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address: " + GetAddressToString(devAddr[i]));
+      Serial.println();
+    }else{
+      Serial.print("Found ghost device at ");
+      Serial.print(i, DEC);
+      Serial.print(" but could not detect address. Check power and cabling");
+    }
+
+    //Get resolution of DS18b20
+    Serial.print("Resolution: ");
+    Serial.print(DS18B20.getResolution( devAddr[i] ));
+    Serial.println();
+
+    //Read temperature from DS18b20
+    tempDev[i] = DS18B20.getTempC( devAddr[i] );
+    Serial.print("Temp C: ");
+    Serial.println(tempDev[i]);
+  }
+}
+
+//Loop measuring the temperature
+void TempLoop(long now){
+  if( now - lastTemp > durationTemp ){ //Take a measurement at a fixed time (durationTemp = 5000ms, 5s)
+    for(int i=0; i<numberOfDevices; i++){
+      float tempC = DS18B20.getTempC( devAddr[i] ); //Measuring temperature in Celsius
+      tempDev[i] = tempC; //Save the measured value to the array
+      /*Serial.print("Temp");
+      Serial.print(i);
+      Serial.print("= ");
+      Serial.println(tempC);*/
+    }
+    DS18B20.setWaitForConversion(false); //No waiting for measurement
+    DS18B20.requestTemperatures(); //Initiate the temperature measurement
+    lastTemp = millis();  //Remember the last time measurement
+  }
+}
+
